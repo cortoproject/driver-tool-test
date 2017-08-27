1dcb2bd0192e69945595741e9328e31e7c30de79
Sander Mertens <sander.mertens8@gmail.com>
2017-08-27 06:54:15 -0700

    Initial commit

---
+++ include/test.h
@@ -0,0 +1,18 @@
+
+#ifndef TEST_H
+#define TEST_H
+
+/* Add include files here */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Insert definitions here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TEST_H */
+
